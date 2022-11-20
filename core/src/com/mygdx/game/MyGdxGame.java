27d3dff7f677c17efcedb9360630fb5054e5a1aa
monika21169 <monika21169@iiitd.ac.in>
2022-11-20 16:55:11 +0530

    ch

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,37 +1,47 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import org.w3c.dom.Text;
 
-public class MyGdxGame extends Actor implements ApplicationListener {
+public class MyGdxGame implements ApplicationListener {
+//
+//	Animation animation;
+//	Object reg;
+//	float stateTime;
+	private Texture Loading1;
+	private Texture Loading2;
+	private Texture Loading3;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
 
-	Animation animation;
-	Object reg;
-	float stateTime;
-
-	public MyGdxGame() {
-		this.animation = animation;
-		reg = animation.getKeyFrame(0);
-	}
-
-	@Override
-	public void act(float delta) {
-		stateTime += delta;
-		reg = animation.getKeyFrame(stateTime);
-	}
-	public void draw(SpriteBatch batch, float parentAlpha) {
-		batch.draw((Texture) reg, getX(), getY());
-	}
+//	public MyGdxGame() {
+//		this.animation = animation;
+//		reg = animation.getKeyFrame(0);
+//	}
+//
+//	@Override
+//	public void act(float delta) {
+//		stateTime += delta;
+//		reg = animation.getKeyFrame(stateTime);
+//	}
+//	public void draw(SpriteBatch batch, float parentAlpha) {
+//		batch.draw((Texture) reg, getX(), getY());
+	//}
 
 	@Override
 	public void create() {
-		Texture Loading1;
-		Texture Loading2;
-
+		Loading1=new Texture(Gdx.files.internal("Loading1.jpg"));
+		Loading2=new Texture(Gdx.files.internal("Loading2.jpg"));
+		Loading3=new Texture(Gdx.files.internal("Loading3.png"));
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		batch = new SpriteBatch();
 	}
 
 	@Override
